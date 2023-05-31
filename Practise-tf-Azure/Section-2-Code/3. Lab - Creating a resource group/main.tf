278b87ac2d71273144c677672ce3b85c18d028d7
Divyanshu <divyanshursahu@gmail.com>
2023-05-31 13:02:32 +0530

    New files

--- Practise-tf-Azure/Section-2-Code/3. Lab - Creating a resource group/main.tf
+++ Practise-tf-Azure/Section-2-Code/3. Lab - Creating a resource group/main.tf
@@ -1,8 +1,14 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name = "div-terraform-sf-backend"
+    storage_account_name = "terraformsfbackend"
+    container_name = "value"
+    key = "dev.terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "3.8.0"
+      version = "3.58.0"
     }
   }
 }
