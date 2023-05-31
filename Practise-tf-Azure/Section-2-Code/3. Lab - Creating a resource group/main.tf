eb346210787ce948a65d2f976b422fe3b2ccfce6
Divyanshu <divyanshursahu@gmail.com>
2023-05-31 12:31:09 +0530

    removing Comments

--- Practise-tf-Azure/Section-2-Code/3. Lab - Creating a resource group/main.tf
+++ Practise-tf-Azure/Section-2-Code/3. Lab - Creating a resource group/main.tf
@@ -1,12 +1,3 @@
-/*
-
-The following links provide the documentation for the new blocks used
-in this terraform configuration file
-
-1. azurerm_resource_group - https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
-
-*/
-
 terraform {
   required_providers {
     azurerm = {
