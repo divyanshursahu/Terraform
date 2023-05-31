cc1591e8cce077734b0698cef391260882a9ab19
Divyanshu <divyanshursahu@gmail.com>
2023-05-31 14:32:08 +0530

    Made changes in for storage account

--- Practise-tf-Azure/Section-2-Code/5. Lab - Creating an Azure Storage account/main.tf
+++ Practise-tf-Azure/Section-2-Code/5. Lab - Creating an Azure Storage account/main.tf
@@ -8,31 +8,33 @@ in this terraform configuration file
 */
 
 terraform {
+  backend "azurerm" {
+    resource_group_name = "div-terraform-sf-backend"
+    storage_account_name = "terraformsfbackend"
+    container_name = "tfstatefile"
+    key = "dev.terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "3.8.0"
+      version = "3.58.0"
     }
   }
 }
 
 provider "azurerm" {
-  subscription_id = "6912d7a0-bc28-459a-9407-33bbba641c07"
-  tenant_id = "70c0f6d9-7f3b-4425-a6b6-09b47643ec58"
-  client_id = "fb3eb97c-8679-488e-a4b4-5fd90299b7a2"
-  client_secret = "38o8Q~MsAPo59FK.Zsq0eQImB-vW2wrJznAUqc9V"
-  features {}  
+  features {}
 }
 
 resource "azurerm_resource_group" "appgrp" {
   name     = "app-grp"
-  location = "North Europe"
+  location = "West Europe"
 }
 
 resource "azurerm_storage_account" "appstore566565637" {
   name                     = "appstore566565637"
   resource_group_name      = "app-grp"
-  location                 = "North Europe"
+  location                 = "West Europe"
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind = "StorageV2"
