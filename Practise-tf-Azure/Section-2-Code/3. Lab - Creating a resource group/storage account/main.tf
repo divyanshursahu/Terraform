ee7ac3f4872178d87a2e29d2534973c61959803a
Divyanshu <divyanshursahu@gmail.com>
2023-06-01 18:45:41 +0530

    new file

---
+++ Practise-tf-Azure/Section-2-Code/3. Lab - Creating a resource group/storage account/main.tf
@@ -0,0 +1,8 @@
+resource "azurerm_storage_account" "sa36874hjh38223" {
+  name                     = "sa36874hjh38223"
+  resource_group_name      = "app-grp"
+  location                 = "West Europe"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind = "StorageV2"
+}
