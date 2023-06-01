1633f3789e7b27af9e63d58b5cbe02ed40c77e8a
Divyanshu <divyanshursahu@gmail.com>
2023-06-01 18:50:37 +0530

    code push

--- Practise-tf-Azure/Section-2-Code/3. Lab - Creating a resource group/storage account/main.tf
+++ Practise-tf-Azure/Section-2-Code/3. Lab - Creating a resource group/storage account/main.tf
@@ -1,3 +1,6 @@
+provider "azurerm" {
+  features {}
+}
 resource "azurerm_storage_account" "sa36874hjh38223" {
   name                     = "sa36874hjh38223"
   resource_group_name      = "app-grp"
