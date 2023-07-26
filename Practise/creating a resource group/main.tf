4a0a968cfa23a8835493e66884ac98ec677be0f3
divyanshu <divyanshu.shahu@outlook.com>
2023-07-26 13:53:57 +0530

    new files

--- Practise/creating a resource group/main.tf
+++ Practise/creating a resource group/main.tf
@@ -1,4 +1,22 @@
+# module "provider" {
+#   #source = "./Users/divyanshusahu/Downloads/Github/Terraform/Practise"
+#   source = "../provider"
+# }
+
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "3.66.0"
+    }
+  }
+}
+
+provider "azurerm"{
+  features {}
+}
+
 resource "azurerm_resource_group" "app-rg" {
   name = "app-rg"
-  location = "EAST US"
+  location = "East US"
 }
